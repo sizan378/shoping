e45c4b2fda2ba6436c5cca3d981b8913c23542d8
sizan378 <sizanmahmud08@gmail.com>
2022-05-12 17:51:27 +0600

    add static file urls code

--- shoping/settings.py
+++ shoping/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = '/static/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = '/media/'
 
 LOGIN_REDIRECT_URL ='/profile/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
