f83d461fd8250c94d18ac0a2ba7b3ed937d9a31d
sizan378 <sizanmahmud08@gmail.com>
2022-05-12 18:13:19 +0600

    add whitenoise setting

--- shoping/settings.py
+++ shoping/settings.py
@@ -48,9 +48,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'shoping.urls'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATES = [
     {
